f3aee91dd84b8ae760aae7b587a4b8243ebdbf66
Wouter Dijk <wtrdk@WtrdksSqromatic.fritz.box>
2018-10-15 13:39:08 +0200

    First

---
+++ mp3cut.py
@@ -0,0 +1,31 @@
+#!/bin/python
+from sys  import argv
+from os   import system
+from subprocess import Popen, PIPE
+
+ffm = 'ffmpeg -i' # input file
+aud = ' -acodec mp3' #add your quality preferences
+dur = ' 2>&1 | grep "Duration" | cut -d " " -f 4'
+
+def cutter(inp,t=0):
+  out = inp[:-5] + '_cut' + inp[-5:]
+  cut = ' -t %s' % ( duration(inp)-t )
+  cmd = ffm + inp + aud + cut + out
+  print cmd;  system(cmd)
+
+def fader(inp,t=0):
+  out = inp[:-5] + '_fade' + inp[-5:]
+  fad = ' -af "afade=t=out:st=%s:d=%s"' % ( duration(inp)-t, t )
+  cmd = ffm + inp + fad + out
+  print cmd;  system(cmd)
+
+def duration(inp):
+  proc = Popen(ffm + inp + dur, shell=True, stdout=PIPE, stderr=PIPE)
+  out,err = proc.communicate()
+  h,m,s = [float(x)  for x in out[:-2].split(':')]
+  return (h*60 + m)*60 + s
+
+if __name__ == '__main__':
+  fname=' "'+argv[1]+'"'
+  cutter(fname,10)
+  fader (fname, 5)
